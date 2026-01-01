9ff9b21934b48370ee3d1d74fcf0b276e2feaab5
Zhelair <nik.sales.737@gmail.com>
2026-01-01 23:36:50 +0200

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.tabs button').forEach(btn=>{
-  btn.onclick=()=>{
-    document.querySelectorAll('.tabs button').forEach(b=>b.classList.remove('active'));
-    document.querySelectorAll('.tab').forEach(t=>t.classList.remove('active'));
-    btn.classList.add('active');
-    document.getElementById(btn.dataset.tab).classList.add('active');
-  };
-});
