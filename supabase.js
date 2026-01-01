4e88f22cefcdde028e6b139213bcb8910b9ba15e
Zhelair <nik.sales.737@gmail.com>
2026-01-01 23:36:43 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = "https://qcmyfmmbdrxqjkzqtpxy.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjbXlmbW1iZHJ4cWprenF0cHh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcyOTA1OTIsImV4cCI6MjA4Mjg2NjU5Mn0.XxC5ISCfKExN6Yd-WK4x2dAfD9JMnGneC_urbsCow6w";
-
-window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
