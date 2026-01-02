224399ed1c31a6e24da6d5b7c2a043da3fe94b30
Zhelair <nik.sales.737@gmail.com>
2026-01-02 03:38:13 +0200

    Update Supabase URL and anon key

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@
 // Paste your Supabase Project URL + anon public key (Project Settings → API).
 // Never use service_role key in frontend.
 
-const SUPABASE_URL = "PASTE_SUPABASE_URL_HERE";
-const SUPABASE_ANON_KEY = "PASTE_SUPABASE_ANON_KEY_HERE";
+const SUPABASE_URL = "https://ubthnjsdxuhjyjnrxube.supabase.co";
+const SUPABASE_ANON_PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVidGhuanNkeHVoanlqbnJ4dWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY1Njc1OTIsImV4cCI6MjA4MjE0MzU5Mn0.zOUuQErKK2sOhIbmG2OVbwBkuUe3TfrEEGBlH7-dE_g";;
 
 window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
